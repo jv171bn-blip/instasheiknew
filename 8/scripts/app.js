c0efc765b33f0b3ec32942c965e5a32564d61cd8
jv171bn <jv171bn@gmail.com>
2026-01-14 06:10:21 -0300

    Atualizações no projeto

--- 8/scripts/app.js
+++ 8/scripts/app.js
@@ -1,14 +1,4 @@
 (() => {
-  if (!window.fbq) {
-    (function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-    n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)})(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-  }
-  try {
-    window.fbq('init', '1246894817453773');
-    window.fbq('track', 'PageView');
-  } catch(e) {}
   const modal = document.getElementById('pix-modal');
   console.log('modal:', modal);
   const closeBtn = modal.querySelector('.modal-close');
@@ -153,10 +143,6 @@
     qrSkeleton.style.display = 'block';
     pixCodeInput.value = '';
     createPixTransaction();
-    if (!initiateCheckoutDisparado && window.fbq) {
-      fbq('track', 'InitiateCheckout');
-      initiateCheckoutDisparado = true;
-    }
   };
   if (ctaBtn && !ctaBtn.hasAttribute('onclick')) {
     ctaBtn.addEventListener('click', window.abrirPopupPix);
